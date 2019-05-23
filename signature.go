8245b9c8bac8676a331d4dbc4c2c121eec81305f
Dung Pham <tridungpham.89@gmail.com>
2019-05-24 00:32:16 +0700

    WIP

--- signature.go
+++ signature.go
@@ -6,68 +6,57 @@ import (
 	"encoding/hex"
 	"fmt"
 	"hash"
-	"reflect"
-	"sort"
 )
 
-func sortKeys(arr interface{}) []string {
-	v := reflect.ValueOf(arr)
-	if v.Kind() != reflect.Map {
-		return make([]string, 0)
-	}
+func CalculateSign(
+	key string,
+	params map[string]string,
+	version int) string {
+	hasher := getHasher(version)
+	stringToHash := getStringToHash(version, params)
+	stringToHash += key
 
-	allKeys := reflect.ValueOf(arr).MapKeys()
-	keys := make([]string, len(allKeys))
-
-	for _, v := range allKeys {
-		k := v.String()
-		if len(k) > 0 {
-			keys = append(keys, k)
-		}
-	}
-
-	sort.Strings(keys)
-	return keys
+	hasher.Write([]byte(stringToHash))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-func prepareParameters(data map[string]interface{}, baseString string) string {
-	base := ""
-	keys := sortKeys(data)
-	for _, key := range keys {
-		value := data[key]
-		switch t := value.(type) {
-		case nil:
-			continue
-		case map[string]string:
-			ks := sortKeys(t)
-			for _, k := range ks {
-				if len(k) == 0 {
-					continue
-				}
-				v := t[k]
-				base += key + "[" + k + "]" + "=" + v
-			}
-		default:
-			base += key + "=" + fmt.Sprint(t)
-		}
-
-	}
-
-	return base + baseString
-}
-
-func CalculateSignature(privateKey string, data map[string]interface{}, version int) string {
-	str := prepareParameters(data, privateKey)
+func getHasher(version int) hash.Hash {
 	var hasher hash.Hash
+
 	switch version {
 	case 1, 2:
 		hasher = md5.New()
+		break
+
 	case 3:
 		hasher = sha256.New()
-	default:
-		panic("invalid signature version")
 	}
 
-	hasher.Write([]byte(str))
-	return hex.EncodeToString(hasher.Sum(nil))
+	return hasher
+}
+
+func getStringToHash(version int, params map[string]string) string {
+	switch version {
+	case 1:
+		if uid, ok := params["uid"]; ok {
+			return uid
+		}
+
+		return ""
+
+	case 2, 3:
+		params = SortParameters(params)
+		return mapToHashString(params)
+	}
+
+	return ""
+}
+
+func mapToHashString(params map[string]string) string {
+	base := ""
+	for key, v := range params {
+		base += fmt.Sprintf("%s=%s", key, v)
+	}
+
+	return base
 }
--- signature_test.go
+++ signature_test.go
@@ -1,21 +1,77 @@
-package paymentwall
+package paymentwall_test
 
-import "testing"
+import (
+	"testing"
 
-func TestCalculateSignature(t *testing.T) {
-	privateKey := "my-private-key"
-	data := make(map[string]interface{})
+	"github.com/paymentwall/paymentwall-go"
+)
 
-	data["key1"] = "Hello"
-	data["key2"] = 2
-	data["key3"] = map[string]string{"key4": "hello", "key5": "world", "abc": "def"}
-	data["key0"] = "yolo"
-	data["key11"] = nil
-	data["key111"] = 1.2
-
-	correct := "d8aa8cf7e9233345745902cc0278d587c1750c2faa5fe09ac99ff5fd161049e1"
-	sign := CalculateSignature(privateKey, data, 3)
-	if sign != correct {
-		t.Error("Expected: ", correct, " got ", sign)
+func TestCalculateSign(t *testing.T) {
+	type args struct {
+		key     string
+		params  map[string]string
+		version int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test signature version 1",
+			args: args{
+				key: "some-secret-key",
+				params: map[string]string{
+					"name": "alex",
+					"age":  "30",
+				},
+				version: 1,
+			},
+			want: "3b944829fd7589ffed0f66760243e82b",
+		},
+		{
+			name: "test signature version 1 with user id",
+			args: args{
+				key: "some-secret-key",
+				params: map[string]string{
+					"name": "alex",
+					"age":  "30",
+					"uid":  "user-1",
+				},
+				version: 1,
+			},
+			want: "f3151489ee36cfd467b7f56bd89e2b1c",
+		},
+		{
+			name: "test signature version 2",
+			args: args{
+				key: "some-secret-key",
+				params: map[string]string{
+					"name": "alex",
+					"age":  "30",
+				},
+				version: 2,
+			},
+			want: "c4d3d531e1f87cd5155c02bd9703ec3c",
+		},
+		{
+			name: "test signature version 3",
+			args: args{
+				key: "some-secret-key",
+				params: map[string]string{
+					"name": "alex",
+					"age":  "30",
+				},
+				version: 3,
+			},
+			want: "fee0b06d951f2511bc6e6b86c0a30d44b178e074359ae7f3a366d87f6179bae4",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := paymentwall.CalculateSign(tt.args.key, tt.args.params, tt.args.version); got != tt.want {
+				t.Errorf("CalculateSign() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
